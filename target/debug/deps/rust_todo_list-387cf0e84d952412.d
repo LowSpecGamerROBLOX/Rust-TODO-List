918da696afd21ec5cab61ae5311c21717b86e8b0
lil bobert#4957 <78571856+LowSpecGamerROBLOX@users.noreply.github.com>
2023-04-13 18:57:16 -0500

    setup cargo.toml

---
+++ target/debug/deps/rust_todo_list-387cf0e84d952412.d
@@ -0,0 +1,5 @@
+/Users/guillermo/Developer/Public/Rust TODO List/target/debug/deps/rust_todo_list-387cf0e84d952412.rmeta: src/main.rs
+
+/Users/guillermo/Developer/Public/Rust TODO List/target/debug/deps/rust_todo_list-387cf0e84d952412.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/rust_todo_list-7de5ffb8b6218bcf.d
@@ -0,0 +1,5 @@
+/Users/guillermo/Developer/Public/Rust TODO List/target/debug/deps/rust_todo_list-7de5ffb8b6218bcf.rmeta: src/main.rs
+
+/Users/guillermo/Developer/Public/Rust TODO List/target/debug/deps/rust_todo_list-7de5ffb8b6218bcf.d: src/main.rs
+
+src/main.rs:
